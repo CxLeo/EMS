077dc178961aee8958620d98d014019022fdab84
Chenxuan Wang <cxleoo9@gmail.com>
2023-07-21 21:43:44 -0600

    upload backend

---
+++ EMS-backend/src/test/java/com/exercise/myEMS/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.myEMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
